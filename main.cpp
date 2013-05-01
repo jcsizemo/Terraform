986f14b0b1970c056ee5cdabe06c9c410fe6751c
Benevolous <jcsizemo@gmail.com>
2013-05-01 16:37:45 -0400

    Created project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: John
+ *
+ * Created on May 1, 2013, 4:26 PM
+ */
+
+#include <cstdlib>
+#include <GL/glew.h>
+#include <GLUT/glut.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
